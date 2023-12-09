abe428617086683c484e9756476be644028b99b6
VladRaciulete <52530702+VladRaciulete@users.noreply.github.com>
2023-12-10 01:20:05 +0200

    Added Prediction logic and statistics

---
+++ PredictorPerceptronSimplu/Cell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PredictorPerceptronSimplu
+{
+    internal class Cell
+    {
+        public int Taken;
+        public int NotTaken;
+
+        public Cell(int taken, int notTaken)
+        {
+            Taken = taken;
+            NotTaken = notTaken;
+        }
+    }
+
+}
